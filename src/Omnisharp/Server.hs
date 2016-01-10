003d8aee68d31a30c12022e137a1f9f9f6dce4f6
Alistair Bush <alistair.bush@gmail.com>
2016-01-10 13:56:31 +1300

    Add lookup types functionality.

--- src/Omnisharp/Server.hs
+++ src/Omnisharp/Server.hs
@@ -10,15 +10,20 @@ module Omnisharp.Server (
 
 import Control.Monad.IO.Class
 import qualified Data.ByteString as S
+import Data.ByteString.Lazy (toStrict)
+import Data.Aeson
 import Data.Maybe
+import Data.Monoid
+import qualified Data.Text as T
 import Data.Word (Word16)
-import System.IO.Streams (InputStream, OutputStream, stdout)
+import System.IO.Streams (InputStream, OutputStream, stdout, makeInputStream)
 import System.IO.Streams.Attoparsec(ParseException)
+import System.IO.Streams.ByteString
 import qualified System.IO.Streams as Streams
 import System.IO.Temp
 import System.Process
 import Control.Exception (try, IOException, SomeException(SomeException))
-import Data.Typeable (typeOf)
+--import Data.Typeable (typeOf)
 
 import Network.Http.Client hiding (Port)
 
@@ -29,12 +34,33 @@ type Port = Word16
 
 data Server = ServerProcess ProcessHandle Port
 
---data CodeActionRequest = CodeActionRequest {
---    line :: Int,
---    column :: Int,
---    filename :: FilePath,
---    buffer :: S.ByteString
---  }
+data CodeActionRequest = CodeActionRequest {
+  buffer :: T.Text,
+  column :: Int,
+  filename :: FilePath,
+  includeDocumentation :: Bool,
+  line :: Int
+} deriving Show
+
+instance ToJSON CodeActionRequest where
+  toJSON (CodeActionRequest b c f i l) =
+    object ["buffer" .= b, "column" .= (show c), "filename" .= file, "includeDocumentation" .= i, "line" .= (show l)]
+      where
+        file = T.pack f
+
+--  toEncoding (CodeActionRequest b c f i l) =
+--    pairs ("buffer" .= b <> "column" .= c <> filename .= f <> "includeDocumentation" .= i <> "line" .= l)
+
+data TypeLookupResponse = TypeLookupResponse {
+  codetype :: Maybe T.Text,
+  documentation :: Maybe T.Text
+} deriving Show
+
+instance FromJSON TypeLookupResponse where
+  parseJSON (Object v)  = TypeLookupResponse <$>
+                          v .: "Type" <*>
+                          v .: "Documentation"
+  parseJSON _           = mempty
 
 localhost = "127.0.0.1"
 
@@ -47,7 +73,7 @@ startServer p f = do
                                  { std_out = UseHandle h})
 
 stopServer :: Server -> IO ()
-stopServer (ServerProcess ph port) =  withConnection (conn) $ (\c -> do
+stopServer (ServerProcess _ port) =  withConnection (conn) $ (\c -> do
     q <- buildRequest $ do
       http GET "/stopserver"
 
@@ -61,7 +87,7 @@ stopServer (ServerProcess ph port) =  withConnection (conn) $ (\c -> do
     conn = openConnection localhost port
 
 getAliveStatus :: Server -> IO Bool
-getAliveStatus (ServerProcess ph port) = withConnection (conn) $ (\c -> do
+getAliveStatus (ServerProcess _ port) = withConnection (conn) $ (\c -> do
     q <- buildRequest $ do
       http GET "/checkalivestatus"
 
@@ -71,5 +97,15 @@ getAliveStatus (ServerProcess ph port) = withConnection (conn) $ (\c -> do
 --              Left e -> False
 --              Right r -> r == "true"
     return $ input == "true")
+  where
+    conn = openConnection localhost port
+
+lookupType :: Server -> CodeActionRequest -> IO TypeLookupResponse
+lookupType (ServerProcess _ port) req = withConnection (conn) $ (\c -> do
+    q <- buildRequest $ do
+      http POST "/typelookup"
+    body <- fromLazyByteString $ encode req
+    sendRequest c q $ inputStreamBody body
+    receiveResponse c jsonHandler :: IO TypeLookupResponse)
   where
     conn = openConnection localhost port
