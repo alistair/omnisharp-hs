b82bf87744245d38a87ebb7e021b0db76f984440
Alistair Bush <alistair.bush@gmail.com>
2016-01-18 19:24:09 +1300

    get lines from vim buffer.

--- src/Omnisharp/Neovim/Lib.hs
+++ src/Omnisharp/Neovim/Lib.hs
@@ -46,26 +46,41 @@ getStatusPlugin = do
 
 getTypePlugin :: CommandArguments -> Neovim r (Maybe S.Server) ()
 getTypePlugin cargs = do
+    serverM <- get
     (l,c) <- getCurrentPosition
     buffer <- getCurrentBuffer
-    serverM <- get
     req <- S.CodeActionRequest
               <$> return buffer
               <*> return ( (read . show) l)
               <*> return ((read . show) c)
               <*> return "/home/alistair/Projects/blackdunes.chronicles/src/blackdunes.chronicles/Startup.cs"
               <*> return False
-    (S.TypeLookupResponse t d) <- liftIO $ case serverM of 
+    (S.TypeLookupResponse t d) <- liftIO $ case serverM of
                                           (Just server) -> S.lookupType server req
     case t of
       (Just a)  -> vim_out_write $ T.unpack a
       (Nothing) -> vim_out_write "Unable to determine type."
 
 
+getLines :: Buffer -> Int64 -> Neovim t st T.Text
+getLines b i = do
+    lines <- sequence $ fmap (buffer_get_line b) [1..i]
+    return (T.unlines $ makeString lines)
+    where
+      makeString l = case sequence l of Right a -> fmap T.pack a
+                                        Left e -> []
+
+getNumberOfLines :: Buffer -> Neovim t st Int64
+getNumberOfLines b = do
+    count <- buffer_line_count b
+    let c = case count of Right a -> a
+                          Left e -> 0
+    return c
+
 getCurrentBuffer :: Neovim r st T.Text
 getCurrentBuffer = vim_get_current_window >>= window_get_buffer >>= convert
     where
-      convert = \x -> case x of Right r -> case r of Buffer b -> return $ decodeUtf8 b
+      convert x = case x of Right r -> case r of Buffer b -> return $ decodeUtf8 b
 
 getCurrentPosition :: Neovim r st (Int64, Int64)
 getCurrentPosition = extract <$> positionM
